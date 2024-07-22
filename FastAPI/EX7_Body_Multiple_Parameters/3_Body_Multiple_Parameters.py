cf7ecc982e7fc7456adcf2677752283de6ed42a9
daothihuyen64 <daothihuyennv64@gmail.com>
2024-07-22 13:30:19 +0700

    Research - Week2

---
+++ FastAPI/EX7_Body_Multiple_Parameters/3_Body_Multiple_Parameters.py
@@ -0,0 +1,45 @@
+from typing import Annotated
+
+from fastapi import Body, FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+
+
+class User(BaseModel):
+    username: str
+    full_name: str | None = None
+
+
+@app.put("/items/{item_id}")
+async def update_item(
+    item_id: int, item: Item, user: User, importance: Annotated[int, Body()]
+):
+    results = {"item_id": item_id, "item": item, "user": user, "importance": importance}
+    return results
+
+# 3 tham số : item, user, importance đều là request body
+# tham số mô hình : item, user
+# tham số đơn : importance - vì là tham số đơn nên để tránh bị nhầm thành query parameter thêm Body()
+
+# file JSON :
+# {
+#     "item": {
+#         "name": "Foo",
+#         "description": "The pretender",
+#         "price": 42.0,
+#         "tax": 3.2
+#     },
+#     "user": {
+#         "username": "dave",
+#         "full_name": "Dave Grohl"
+#     },
+#     "importance": 5
+# }
